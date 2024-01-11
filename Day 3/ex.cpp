bc62423b922d8cd877db4d0fc73728a2e01cfaab
cear0004 <cear0004@student.monash.edu>
2024-01-11 16:04:55 +1100

    completed day 3 exercises.

---
+++ Day 3/ex.cpp
@@ -0,0 +1,11 @@
+int main()
+{
+    short age = 99;                          // a
+    short backyardArea = 100;                // b
+    long long starsInGalaxy = 1000000000000; // c
+    short averageRainfall = 4;               // d
+
+    const float PI = 3.14159; // part 3-4
+
+    return 0;
+}
